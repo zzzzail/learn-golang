e5d8f1d991d732301428913432cded95fe54f183
Zail <878773559@qq.com>
2019-07-04 15:18:08 +0800

    23

---
+++ tour/23-if.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+if 和 else
+在 if 的短语句中声明的变量同样可以在任何对应的 else 块中使用.
+（在 main 的 fmt.Println 调用开始前，两次对 pow 的调用均已执行并返回其各自的结果。）
+*/
+func main() {
+
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+
+}
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// 这里开始就不能使用 v 变量了
+	return lim
+}
