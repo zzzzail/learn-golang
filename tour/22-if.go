0bc7701d21aeeb231bb2b1377ebd1c69af829e46
Zail <878773559@qq.com>
2019-07-04 15:02:33 +0800

    22

---
+++ tour/22-if.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+if 的简短语句
+
+同 for 一样, if 语句可以在条件表达式之前执行一个简单的语句.
+该语句声明的变量作用域仅在 if 之内.
+(在最后的 return 语句处使用 v 看看)
+*/
+func main() {
+
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+
+}
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
